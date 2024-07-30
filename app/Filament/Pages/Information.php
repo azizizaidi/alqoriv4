955f20f95b9b5d015656b06324532d93483ea53a
azizizaidi <azizikuis@gmail.com>
2024-07-30 21:45:32 +0800

    updated all to teacher part

--- app/Filament/Pages/Information.php
+++ app/Filament/Pages/Information.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use Livewire\Livewire;
 
 class Information extends Page
 {
@@ -10,19 +11,27 @@ class Information extends Page
 
     protected static string $view = 'filament.pages.information';
 
-    public static function table(Table $table): Table
-    {
-        return $table
-    
-            ->emptyStateIcon('heroicon-o-bookmark');
-    
-    }
-    
     public static function getNavigationLabel(): string
     {
         return __('Info');
     }
-    
+
+    public static function getNavigationBadge(): ?string
+    {
+        return (string) static::getBadgeCount();
+    }
+
+    public static function getBadgeCount(): ?int
+    {
+        // Here you should retrieve the badge count, for example from a cache or a database
+        return 1; // Example static count
+    }
+
+    public static function getNavigationBadgeColor(): ?string
+    {
+        return 'warning';
+    }
+
     public function getHeading(): string
     {
         return __('Info');
