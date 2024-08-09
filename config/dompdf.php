b3f25de76fb9b08d5bf0dc05cdf3067faeafc54b
azizizaidi <azizikuis@gmail.com>
2024-08-09 20:05:34 +0800

    pdf file

--- config/dompdf.php
+++ config/dompdf.php
@@ -104,7 +104,7 @@ return array(
         /**
          * Whether to enable font subsetting or not.
          */
-        "enable_font_subsetting" => false,
+        "enable_font_subsetting" => true,
 
         /**
          * The PDF rendering backend to use
@@ -245,7 +245,7 @@ return array(
          *
          * @var bool
          */
-        "enable_javascript" => true,
+        "enable_javascript" => false,
 
         /**
          * Enable remote file access
